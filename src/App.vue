807658a2293a15be48a4f9fc2756244938202842
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-11-10 17:10:07 +0900

    Router - transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -19,5 +21,13 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <p>{{ item.title }}</p>
-    <div>{{ item.content }}</div>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+          <div class="time">{{ item.time_ago }}</div>
+        </div>
+      </div>
+      <h2>{{ item.title }}</h2>
+    </section>
+    <section>
+      <div v-html="item.content"></div>
+    </section>
   </div>
 </template>
 
@@ -19,5 +32,18 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 </style>
