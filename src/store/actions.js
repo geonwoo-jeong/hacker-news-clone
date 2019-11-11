7636df99ae88fc8c78f105da15810f4b44fca991
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-11-11 17:28:44 +0900

    bug fix - Method Chaining

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,16 @@
 import { fetchUserInfo, fetchItemInfo, fetchList } from "../api";
 
 export default {
-  FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName).then(({ data }) => commit("SET_LIST", data));
+  async FETCH_LIST({ commit }, pageName) {
+    return fetchList(pageName).then(({ data }) => commit("SET_LIST", data));
   },
-  FETCH_USER_INFO({ commit }, user) {
-    fetchUserInfo(user).then(response =>
+  async FETCH_USER_INFO({ commit }, user) {
+    return fetchUserInfo(user).then(response =>
       commit("SET_USER_INFO", response.data)
     );
   },
-  FETCH_ITEM_INFO({ commit }, item) {
-    fetchItemInfo(item).then(response =>
+  async FETCH_ITEM_INFO({ commit }, item) {
+    return fetchItemInfo(item).then(response =>
       commit("SET_ITEM_INFO", response.data)
     );
   }
